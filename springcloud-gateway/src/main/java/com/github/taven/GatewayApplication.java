13743f6eafa23f94a8f472828b9ac0e5171b75ed
tianwen.yin <tianwen.yin@openjawtech.com>
2021-06-03 18:07:42 +0800

    gateway demo

---
+++ springcloud-gateway/src/main/java/com/github/taven/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.github.taven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/hello-gateway")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+}
---
+++ springcloud-gateway/src/main/java/com/github/taven/route/NacosRouteDefinitionRepository.java
@@ -0,0 +1,23 @@
+package com.github.taven.route;
+
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class NacosRouteDefinitionRepository implements RouteDefinitionRepository {
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+        return null;
+    }
+}
