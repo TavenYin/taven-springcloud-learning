5e18d947caab73ea79875928becb1b482e84d0ca
TavenYin <yintianwen7@qq.com>
2021-07-02 18:02:22 +0800

    add jaeger example

---
+++ jaeger/src/main/java/com/github/taven/Hello.java
@@ -0,0 +1,45 @@
+package com.github.taven;
+
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.internal.JaegerTracer;
+
+public class Hello {
+
+    private final Tracer tracer;
+
+    private Hello(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    private void sayHello(String helloTo) {
+        Span span = tracer.buildSpan("say-hello").start();
+
+        String helloStr = String.format("Hello, %s!", helloTo);
+        System.out.println(helloStr);
+
+        span.finish();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Expecting one argument");
+        }
+        String helloTo = args[0];
+
+        Tracer tracer = initTracer("hello-world");
+        new Hello(tracer).sayHello(helloTo);
+    }
+
+    public static JaegerTracer initTracer(String service) {
+        SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
+        Configuration config = new Configuration(service).withSampler(samplerConfig).withReporter(reporterConfig);
+        return config.getTracer();
+    }
+}
