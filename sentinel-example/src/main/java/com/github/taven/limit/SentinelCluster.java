5858cba1e458572afcc839f844edfaca9ab9efb0
TavenYin <yintianwen7@qq.com>
2021-07-22 22:18:55 +0800

    update SentinelCluster

--- sentinel-example/src/main/java/com/github/taven/limit/SentinelCluster.java
+++ sentinel-example/src/main/java/com/github/taven/limit/SentinelCluster.java
@@ -5,7 +5,15 @@ import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientAssignConfig;
 import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientConfig;
 import com.alibaba.csp.sentinel.cluster.client.config.ClusterClientConfigManager;
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+import java.util.List;
 
 public class SentinelCluster {
     public static void main(String[] args) {
@@ -13,7 +21,7 @@ public class SentinelCluster {
 
         while (true) {
             // 1.5.0 版本开始可以直接利用 try-with-resources 特性
-            try (Entry entry = SphU.entry("HelloWorld")) {
+            try (Entry entry = SphU.entry("cluster-resource")) {
                 // 被保护的逻辑
                 System.out.println("hello world");
             } catch (BlockException ex) {
@@ -24,14 +32,25 @@ public class SentinelCluster {
         }
     }
 
+    private static final String APP_NAME = "appA";
+    private static final String FLOW_POSTFIX = "-flow-rules";
+    private static final String REMOTE_ADDRESS = "localhost:8848";
+    private static final String GROUP_ID = "SENTINEL_GROUP";
+
     private static void initFlowRules() {
-        ClusterClientConfig clientConfig = new ClusterClientConfig();
-        ClusterClientConfigManager.applyNewConfig(clientConfig);
+//        ClusterClientConfig clientConfig = new ClusterClientConfig();
+//        ClusterClientConfigManager.applyNewConfig(clientConfig);
 
         ClusterClientAssignConfig assignConfig = new ClusterClientAssignConfig();
         assignConfig.setServerHost("127.0.0.1");
         assignConfig.setServerPort(11111);
         ClusterClientConfigManager.applyNewAssignConfig(assignConfig);
 
+        // 使用 Nacos 数据源作为配置中心，需要在 REMOTE_ADDRESS 上启动一个 Nacos 的服务
+        ReadableDataSource<String, List<FlowRule>> ds =
+                new NacosDataSource<>(REMOTE_ADDRESS, GROUP_ID, APP_NAME+FLOW_POSTFIX,
+                        source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {}));
+        // 为集群客户端注册动态规则源
+        FlowRuleManager.register2Property(ds.getProperty());
     }
 }
