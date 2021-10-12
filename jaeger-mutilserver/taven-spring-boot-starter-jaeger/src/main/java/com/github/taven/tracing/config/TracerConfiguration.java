1e89982b3db162996b1c6fed853c4378aa4e7ffc
TavenYin <yintianwen7@qq.com>
2021-10-12 22:59:14 +0800

    完善 TracerConfiguration

--- jaeger-mutilserver/taven-spring-boot-starter-jaeger/src/main/java/com/github/taven/tracing/config/TracerConfiguration.java
+++ jaeger-mutilserver/taven-spring-boot-starter-jaeger/src/main/java/com/github/taven/tracing/config/TracerConfiguration.java
@@ -20,12 +20,14 @@ public class TracerConfiguration {
                 SamplerConfiguration.fromEnv()
                         .withType("const")
                         .withParam(1);
-        // 修改 sender 配置 jaeger collector 或者 agent 地址
+
         // 默认会创建 UdpSender
         // 默认 agent 地址 localhost:6831
         SenderConfiguration senderConfiguration =
                 SenderConfiguration
-                        .fromEnv();
+                        .fromEnv()
+                        // 如果是在 localhost 环境的话，可以不指定 endpoint
+                        .withEndpoint("http://192.168.3.25:14268/api/traces");
         ReporterConfiguration reporterConfig =
                 ReporterConfiguration
                         .fromEnv()
