5340d907ad4b1698c164f9221b4b8b4dbcbf4828
TavenYin <yintianwen7@qq.com>
2022-01-06 21:47:54 +0800

    完善 demo

--- springcloud-restTemplate/src/main/java/com/github/taven/rest/controller/TestController.java
+++ springcloud-restTemplate/src/main/java/com/github/taven/rest/controller/TestController.java
@@ -1,15 +1,20 @@
 package com.github.taven.rest.controller;
 
+import com.github.taven.rest.http.RequestConfigHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.config.RequestConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author tianwen.yin
  */
 @RestController
+@Slf4j
 public class TestController {
     @Resource(name = "customCloudRestTemplate")
     private RestTemplate customCloudRestTemplate;
@@ -20,24 +25,32 @@ public class TestController {
     @Resource(name = "restTemplate")
     private RestTemplate restTemplate;
 
+    @GetMapping("rest/normalRequest")
+    public String restNormalRequest() {
+        return restTemplate.getForObject("http://localhost:8080/slowRequest", String.class);
+    }
+
     @GetMapping("custom/normalRequest")
     public String customNormalRequest() {
-        return null;
+        return customTimeoutRestTemplate.getForObject("http://localhost:8080/slowRequest", String.class);
     }
 
     @GetMapping("custom/setTimeout")
     public String customSetTimeout() {
-        return null;
+        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(7000).build();
+        RequestConfigHolder.bind(requestConfig);
+        return customTimeoutRestTemplate.getForObject("http://localhost:8080/slowRequest", String.class);
     }
 
     @GetMapping("cloud/setTimeout")
     public String cloudSetTimeout() {
-        return null;
+        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(7000).build();
+        RequestConfigHolder.bind(requestConfig);
+        return customTimeoutRestTemplate.getForObject("http://localhost:8080/slowRequest", String.class);
     }
 
-    @GetMapping("rest/normalRequest")
-    public String restNormalRequest() {
-        return null;
+    @GetMapping("slowRequest")
+    public void slowRequest() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(100);
     }
-
 }
